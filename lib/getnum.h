36bf02e80516dba3747fdfcead588069bbb87b09
slumio <dmallah474@gmail.com>
2025-09-08 16:46:22 +0530

    created getenum.c

--- lib/getnum.h
+++ lib/getnum.h
@@ -2,5 +2,19 @@
 #define GETNUM_H
 
 
-#define 
-#endif
+#define GN_NONNEG 01 /*value must be >=0*/
+
+#define GN_GT_0   02  /*values must be >0*/
+
+#define GN_ANY_BASE 0100
+#define GN_BASE_8   0200
+#define GN_BASE_16  0400   
+
+
+long getLong (const char * arg,int flags,const char *name);
+long getInt (const char *arg,int flags,const char *name);
+
+
+
+
+#endif   
